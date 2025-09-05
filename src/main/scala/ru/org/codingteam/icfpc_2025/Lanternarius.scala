e3c460be9508b209fc816e958774e1800706c4b3
Igor S. Gerasimov <foxtranigor@gmail.com>
2025-09-05 18:31:29 +0200

    Add stupid random walker

---
+++ src/main/scala/ru/org/codingteam/icfpc_2025/Lanternarius.scala
@@ -0,0 +1,29 @@
+package ru.org.codingteam.icfpc_2025
+
+import scala.util.Random
+
+object Lanternarius {
+  def lanternarius(N: Int): Seq[Int] = {
+    val rand = new Random()
+    val vals = scala.collection.mutable.ArrayBuffer[Int]()
+
+    var prev = rand.nextInt(6)
+    vals += prev
+
+    for (_ <- 1 until N) {
+      if (rand.nextInt(100) < 60 &&
+          (vals.size < 2 || vals(vals.size - 1) != vals(vals.size - 2))) {
+        vals += prev
+      } else {
+        var next = rand.nextInt(6)
+        while (next == prev) {
+          next = rand.nextInt(6)
+        }
+        vals += next
+        prev = next
+      }
+    }
+
+    vals.toSeq
+  }
+}
--- src/main/scala/ru/org/codingteam/icfpc_2025/Main.scala
+++ src/main/scala/ru/org/codingteam/icfpc_2025/Main.scala
@@ -5,6 +5,6 @@ package ru.org.codingteam.icfpc_2025
         case "solve" :: problemName :: Nil =>
             println(s"solving $problemName")
             Ædificium.select(problemName)
-            val result = Ædificium.explore(Seq(Seq(1, 2, 3)))
+            val result = Ædificium.explore(Seq(Lanternarius.lanternarius(54)))
             println(result)
         case _ => println("Usage: sbt run \"solve <problem-name>\"")
