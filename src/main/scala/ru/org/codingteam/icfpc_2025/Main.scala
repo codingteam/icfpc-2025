231447260af689c93438544d36091bc4356c92b2
Igor S. Gerasimov <foxtranigor@gmail.com>
2025-09-05 18:38:21 +0200

    Generate and push ways

--- src/main/scala/ru/org/codingteam/icfpc_2025/Main.scala
+++ src/main/scala/ru/org/codingteam/icfpc_2025/Main.scala
@@ -5,6 +5,6 @@ package ru.org.codingteam.icfpc_2025
         case "solve" :: problemName :: Nil =>
             println(s"solving $problemName")
             Ædificium.select(problemName)
-            val result = Ædificium.explore(Seq(Lanternarius.lanternarius(54)))
+            val result = Ædificium.explore(Lanternarius.lanternariuses(8, 54))
             println(result)
         case _ => println("Usage: sbt run \"solve <problem-name>\"")
