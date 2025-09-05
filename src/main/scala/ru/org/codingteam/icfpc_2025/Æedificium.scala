c25cb5cb478be67df4a4aa7d77a6c0992ad01830
Evgeniy Andreev (gsomix) <gsomix@gmail.com>
2025-09-05 19:45:49 +0400

    chore: more Scala 3 syntax

--- src/main/scala/ru/org/codingteam/icfpc_2025/Æedificium.scala
+++ src/main/scala/ru/org/codingteam/icfpc_2025/Æedificium.scala
@@ -7,34 +7,32 @@ import upickle.ReadWriter
 import java.nio.file.{Files, Path}
 import scala.util.Using
 
-object Ædificium {
+object Ædificium:
 
     private val baseUrl = "https://31pwr5t6ij.execute-api.eu-west-2.amazonaws.com"
 
     private def backend = DefaultSyncBackend()
     private val id =
         val file = "api-secret.txt"
-        if (Files.notExists(Path.of(file)))
+        if Files.notExists(Path.of(file)) then
             throw new Exception(s"File $file not found. Please create it and put your API secret there.")
-        Using.resource(scala.io.Source.fromFile(file)) { source =>
+        Using.resource(scala.io.Source.fromFile(file)): source =>
             val secret = source.getLines().mkString("\n").trim
             secret
-        }
 
     private case class SelectRequest(id: String, problemName: String) derives ReadWriter
     private case class ExploreRequest(id: String, plans: Seq[String]) derives ReadWriter
     private case class ExploreResponse(results: Seq[Seq[Int]], queryCount: Int) derives ReadWriter
 
-    def select(problemName: String): Unit = {
+    def select(problemName: String): Unit =
         val response = basicRequest
             .post(uri"$baseUrl/select")
             .body(asJson(SelectRequest(id, problemName)))
             .send(backend)
         assertSuccess(response)
-    }
 
-    def explore(plans: Seq[Seq[Int]]): Seq[Seq[Int]] = {
-        val request = ExploreRequest(id, plans.map { _.mkString("") })
+    def explore(plans: Seq[Seq[Int]]): Seq[Seq[Int]] =
+        val request = ExploreRequest(id, plans.map(_.mkString("")))
         println(s"Sending request: ${asJson(request)}")
 
         val response = basicRequest
@@ -44,12 +42,9 @@ object Ædificium {
             .send(backend)
         assertSuccess(response)
         response.body.toOption.get.results
-    }
 
-    private def assertSuccess(response: Response[?]): Unit = {
+    private def assertSuccess(response: Response[?]): Unit =
         if response.code.code != 200 then
             throw RuntimeException(s"Request failed with status ${response.code.code}: ${response.body}")
-    }
-}
 
 val AEdificium = Ædificium // accessibility! yay!
