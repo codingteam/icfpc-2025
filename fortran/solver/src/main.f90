7bdfd84ed6ac30c765b66601f6a2045e295addfd
Igor S. Gerasimov <foxtranigor@gmail.com>
2025-09-06 18:47:51 +0200

    Catch incorrect usage

--- fortran/solver/src/main.f90
+++ fortran/solver/src/main.f90
@@ -1,2 +1,13 @@
-program solver
-end program solver
+program icfpc2025solver
+    use solver_mod, only: solver_t
+    implicit none
+    character(len=255) :: arg_
+    character(len=:), allocatable :: arg
+    integer :: nargs
+    nargs = command_argument_count()
+    if (nargs /= 2) then
+        call get_command_argument(0, arg_)
+        arg = trim(arg_)
+        error stop "Use: " // arg // " <file to solve>"
+    end if
+end program icfpc2025solver
