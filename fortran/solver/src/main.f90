b547575133942898456f753dfbb953b1a0fd4355
Igor S. Gerasimov <foxtranigor@gmail.com>
2025-09-06 18:53:46 +0200

    Get filename for solving

--- fortran/solver/src/main.f90
+++ fortran/solver/src/main.f90
@@ -5,9 +5,14 @@ program icfpc2025solver
     character(len=:), allocatable :: arg
     integer :: nargs
     nargs = command_argument_count()
-    if (nargs /= 2) then
+    if (nargs /= 1) then
         call get_command_argument(0, arg_)
         arg = trim(arg_)
         error stop "Use: " // arg // " <file to solve>"
     end if
+    call get_command_argument(1, arg_)
+    if (len_trim(arg_) == 0) then
+        error stop "Empty filename was set"
+    end if
+    arg = trim(arg_)
 end program icfpc2025solver
