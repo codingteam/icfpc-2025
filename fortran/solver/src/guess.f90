c72ad790337788aeeac84ebb872679d2db1f371e
Igor S. Gerasimov <foxtranigor@gmail.com>
2025-09-08 05:18:57 +0200

    Implement random initialisation of guess

--- fortran/solver/src/guess.f90
+++ fortran/solver/src/guess.f90
@@ -15,6 +15,15 @@ module guess_mod
         procedure :: execute_plan
         procedure :: get_mask
     end type simplified_library_t
+    type :: guess_t
+        logical :: inited = .false.
+        type(library_t), pointer :: library
+        integer, allocatable :: guess(:)
+        logical, allocatable :: mask(:)
+    contains
+        procedure :: init => guess_init
+    end type guess_t
+    public :: guess_t
 contains
     subroutine sl_init(library, n_rooms, guess, mask)
         class(simplified_library_t), intent(inout) :: library
@@ -79,4 +88,45 @@ contains
             end do
         end do
     end function get_mask
+    subroutine guess_init(guess, library)
+        use random_mod, only: shuffle
+        class(guess_t), intent(inout) :: guess
+        type(library_t), target, intent(in) :: library
+        integer :: n_rooms, room_id, door_id, cnt, room_in
+        integer, allocatable :: rooms(:)
+
+        if (guess%inited) return
+        guess%inited = .true.
+
+        guess%library => library
+        n_rooms = size(guess%library%rooms)
+        allocate(guess%guess(6*n_rooms), source = -1)
+        allocate(guess%mask(6*n_rooms), source = .false.)
+        allocate(rooms(n_rooms), source = 6)
+
+        cnt = 1
+        do room_id = 1, n_rooms
+            do door_id = 0, 5
+                room_in = guess%library%rooms(room_id)%doors(door_id)%room
+                if (room_in /= -1) then
+                    guess%guess(cnt) = room_in
+                    guess%mask(cnt) = .true.
+                    rooms(room_in) = rooms(room_in) - 1
+                end if
+                cnt = cnt + 1
+            end do
+        end do
+
+        room_id = 1
+        do cnt = 1, size(guess%guess)
+            if (rooms(room_id) == 0) room_id = room_id + 1
+            if (.not.guess%mask(cnt)) then
+                guess%guess(cnt) = room_id
+                rooms(room_id) = rooms(room_id) - 1
+            end if
+        end do
+
+        call shuffle(guess%guess, size(guess%guess), guess%mask)
+
+    end subroutine guess_init
 end module guess_mod
